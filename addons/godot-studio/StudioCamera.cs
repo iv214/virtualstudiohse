1b8855bf2eff74ae4533c1e7ca2c34a88e9278c4
iv214 <ivshitov@edu.hse.ru>
2025-05-22 18:08:50 +0300

    subviewport size export

--- addons/godot-studio/StudioCamera.cs
+++ addons/godot-studio/StudioCamera.cs
@@ -10,25 +10,42 @@ public partial class StudioCamera : Node3D
     private OnvifCameraHookNode _onvifhook;
     
     [Export]
-    public string NDIOutputName  {
+    public string NDIOutputName
+    {
         get => GetNode<Node>("SubViewport/NDIOutput").Get("name").ToString();
         set => GetNode<Node>("SubViewport/NDIOutput").Set("name", value);
     }
     [Export]
-	public string ConnectionIP  {
+	public string ConnectionIP
+    {
         get => GetNode<OnvifCameraHookNode>("SubViewport/Camera3D/OnvifCameraHookNode").Get("ConnectionIP").ToString();
         set => GetNode<OnvifCameraHookNode>("SubViewport/Camera3D/OnvifCameraHookNode").Set("ConnectionIP", value);
     }
 	[Export]
-	public string ConnectionUsername  {
+	public string ConnectionUsername
+    {
         get => GetNode<OnvifCameraHookNode>("SubViewport/Camera3D/OnvifCameraHookNode").Get("ConnectionUsername").ToString();
         set => GetNode<OnvifCameraHookNode>("SubViewport/Camera3D/OnvifCameraHookNode").Set("ConnectionUsername", value);
     }
 	[Export]
-	protected string ConnectionPassword  {
+	protected string ConnectionPassword
+    {
         get => GetNode<OnvifCameraHookNode>("SubViewport/Camera3D/OnvifCameraHookNode").Get("ConnectionPassword").ToString();
         set => GetNode<OnvifCameraHookNode>("SubViewport/Camera3D/OnvifCameraHookNode").Set("ConnectionPassword", value);
     }
+    [Export(PropertyHint.Range, "0,16384,1")]
+    protected int ViewportWidth
+    {
+        get => GetNode<SubViewport>("SubViewport").Size.X;
+        set { GetNode<SubViewport>("SubViewport").Size = new Vector2I(value, GetNode<SubViewport>("SubViewport").Size.Y); }
+    }
+    [Export(PropertyHint.Range, "0,16384,1")]
+    protected int ViewportHeight
+    {
+        get => GetNode<SubViewport>("SubViewport").Size.Y;
+        set { GetNode<SubViewport>("SubViewport").Size = new Vector2I(GetNode<SubViewport>("SubViewport").Size.X, value); }
+    }
+
     public override void _Ready()
     {
         base._Ready();
